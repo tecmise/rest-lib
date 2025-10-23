524d50654352051a3bc40cbec4dd684536859bb6
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-10-23 01:26:05 -0300

    deprecated

--- pkg/validators/validator.go
+++ pkg/validators/validator.go
@@ -1,25 +1,11 @@
 package validators
 
 import (
-	"errors"
 	"github.com/gofiber/fiber/v2"
 )
 
 type ValidationOutputMiddleware[T Validatable] func(c *fiber.Ctx) error
 
+// Deprecated: use github.com/tecmise/validation-lib instead
 type Validatable interface {
-	Validate() error
-}
-
-func JsonBinding(ctx *fiber.Ctx, input interface{}) error {
-	// Faz o parsing do corpo para o input
-	if err := ctx.BodyParser(&input); err != nil {
-		return err
-	}
-	converted, ok := input.(Validatable)
-	if ok {
-		return converted.Validate()
-	} else {
-		return errors.New("error to convert input, is not validatable")
-	}
 }
